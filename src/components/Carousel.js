d245ca21b030de5e4f57b167b42f92413594d713
Anuj Gakhar <anuj.gakhar@gmail.com>
2020-03-26 17:40:13 +0000

    adding some CSS

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import {searchImages} from '../utils/api';
+import { searchImages } from '../utils/api';
 import CarouselItem from './CarouselItem';
 
+function Carousel(props) {
+  const { numberOfItems = 6 , title = 'Carousel test'} = props;
 
+  const [currentItem, setCurrentItem] = useState(0);
+  const [items, setItems] = useState();
 
-function Carousel (props) {
-    const {numberOfItems = 6} = props;
-
-    const [currentItem, setCurrentItem] = useState(0);
-    const [items, setItems] = useState();
-
-
-    useEffect(() => {
-        searchImages({per_page:numberOfItems}).then((response) => {
-            setItems(response.data.hits);
-        });
-    }, 
+  useEffect(
+    () => {
+      searchImages({ per_page: numberOfItems }).then(response => {
+        setItems(response.data.hits);
+      });
+    },
     // eslint-disable-next-line
-    [])
+    []
+  );
 
-    const noItems = () => {
-        return (<div>Fetching items....</div>)
-    }
+  const noItems = () => {
+    return <div>Fetching items....</div>;
+  };
 
-    if (!items) return noItems();
+  if (!items) return noItems();
 
-    return (
-        <div className="carousel">
-            {items.map(item => <CarouselItem item={item} />)}
-        </div>
-    )
-
+  return (
+    <div className="carousel-wrapper">
+      <h2 className="carousel-title">{title}</h2>
+      <div className="carousel">
+        {items.map(item => (
+          <CarouselItem item={item} />
+        ))}
+      </div>
+      <div className="action-bar"></div>
+    </div>
+  );
 }
 
 export default Carousel;
